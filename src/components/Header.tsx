d14ef5d6fccb3b8ba31c4733c9d2b9b447a1d960
yabunodaisuke <d-yokota@asukomu.co.jp>
2022-09-11 11:28:42 +0900

    header終了

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,57 +1,128 @@
-/* eslint-disable jsx-a11y/alt-text */
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+
+import React, { useState } from 'react';
 import {Link as Scroll } from 'react-scroll'
 
 
-
-// eslint-disable-next-line @typescript-eslint/naming-convention
-interface menuList {
-    link: string;
-    name: string;
-}
-
 // eslint-disable-next-line import/no-default-export
-export default function PageHeader(this: any): JSX.Element {
-
-    const menuLists:  menuList  [] = [
+export default function Header () {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const [openMenu, setOpenmenu] = useState(false);
     
-    {link:'Home', name:'Home'},
-    {link:'サービス', name:'Service'},
-    {link:'Price', name:'Price'},
-    {link:'Company', name:'Company'},
-    ];
 
- 
+    const menuFunction = () => {
+        setOpenmenu(!openMenu);
+    }
 
-return (
-          <header className=' bg-primary-800 max-w'>
-              <div className='flex  items-center justify-between'>
-                  <div className='hover:opacity-75 px-10'>
-                      <Link href="/">
-                          <a>
-                          <Image src='/img/fwywd.png' alt='logo' width={200} height={100} /> 
-                          </a>
-                      </Link>
-                  </div>
-                  <div className='md:flex  hidden'>
-                  <ul className='grid grid-cols-4 gap-4 text-white pl-5 text-lg '>
-                      {menuLists.map((value, index) => (
-                          <li className='text-primary-dark hover:opacity-50 hover' key={index}> 
-                          <Scroll to={value.link} smooth={true} offset={-50}>
-                              {value.name}
-                          </Scroll>
+    const menuLists  = [
+    
+            {link:'top', name:'Top'},
+            {link:'悩み', name:'悩み'},
+            {link:'解決策', name:'解決策'},
+            {link:'料金', name:'料金'},
+            ];
+    return (
+<nav className='grid grid-cols-2'>
+      <div>
+        <Link href='/'>
+          <a className='px-5 '>
+            <Image src='/img/fwywd.png' alt='logo' width={200} height={100} />
+          </a>
+        </Link>
+      </div>
+      {openMenu ? (
+        <div className='flex flex-row absolute z-10   min-h-fit min-w-full'>
+          <div className='basis-1/2'></div>
+
+          <div className='basis-1/2 bg-white '>
+            <ul className=' text-center  '>
+              <li className='p-2 '>
+                <button onClick={menuFunction} className='font-light'>
+                  close
+                </button>
+              </li>
+              {menuLists.map((value, index) => (
+                <li key={index} className='p-2 border-primary-500'>
+                  {/* <a href={value.link} onClick={menuFunction}>
+                    {value.name}
+                  </a> */}
+                  <Scroll to={value.link} smooth={true} offset={-50} onClick={menuFunction} >
+                          {value.name}
+                       </Scroll>
+                </li>
+
+              ))}
+            </ul>
+          </div>
+        </div>
+      ) : undefined}
+      <div className='flex-initial text-[#abc5c5]  m-5 '>
+        <ul className='md:flex  hidden flex-initial text-left'>
+          {menuLists.map((value, index) => (
+            <li key={index} className='p-4 text-lg'>
+              <a href={value.link}>{value.name} </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <button onClick={menuFunction} className='flex-initial absolute top-0 right-0 md:hidden'>
+        <Image src='/img/menu.png' alt='menu' width={50} height={50} />
+      </button>
+    </nav>
+    )
+
+}
+// // eslint-disable-next-line @typescript-eslint/naming-convention
+// interface menuList {
+//     link: string;
+//     name: string;
+// }
+
+// // eslint-disable-next-line import/no-default-export
+// export default function PageHeader(this: any): JSX.Element {
+
+//     const menuLists:  menuList  [] = [
+    
+//     {link:'top', name:'Top'},
+//     {link:'悩み', name:'悩み'},
+//     {link:'解決策', name:'解決策'},
+//     {link:'料金', name:'料金'},
+//     ];
+
+   
+
+
+// return (
+//           <header className=' bg-primary-800 max-w'>
+//               <div className='flex  items-center justify-between'>
+//                   <div className='hover:opacity-75 px-10'>
+//                       <Link href="/">
+//                           <a>
+//                           <Image src='/img/fwywd.png' alt='logo' width={200} height={100} /> 
+//                           </a>
+//                       </Link>
+//                   </div>
+//                   <div>
+//                   <ul className='md:grid grid-cols-4 gap-4  hidden text-white pl-5 text-base'>
+//                       {menuLists.map((value, index) => (
+//                           <li className='text-primary-dark hover:opacity-50 hover' key={index}> 
+//                           <Scroll to={value.link} smooth={true} offset={-50}>
+//                               {value.name}
+//                           </Scroll>
                         
-                          </li>
-                      ))}
-                      </ul>
-                  </div>
-                  <div id='button' className='text-white text-center bg-yellow border-2 w-60 h-16  pt-5 mr-5  hover:opacity-50 hover'> <a href="https://docs.google.com/forms/d/e/1FAIpQLSdfxNsbBrL_K6DrCY0ND3zdI23JiQXBy63VYHoM4Iklof8jvA/viewform?usp=sf_link">ログイン</a>
+//                           </li>
+//                       ))}
+//                       </ul>
+//                   </div>
+//                   <div id='button' className=' text-white bg-gradient-to-r from-orange to-yellow w-50 h-16  text-center pt-5 mr-5  hover:opacity-50 hover md:visible invisible'> <a href="https://docs.google.com/forms/d/e/1FAIpQLSdfxNsbBrL_K6DrCY0ND3zdI23JiQXBy63VYHoM4Iklof8jvA/viewform?usp=sf_link">資料ダウンロード</a>
                   
-                  </div>
+//                   </div>
                   
-              </div>
-          </header>
-      )
-                      }
+//                   <button className='md:invisible visible'>
+//                   <Image src='/img/menu.png' alt='logo' width={50} height={50} /> 
+//                   </button>
+//               </div>
+//           </header>
+//       )
+//                       }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,21 +2,38 @@ import Link from "next/link";
 import Header from "@/components/Header";
 
 
-const Home: React.FC = () => (
-  <section className='mx-auto max-w-6xl py-6 bg-beige'>
-    <h1 className='text-lg font-bold text-primary-800'>
+export default function Home() {
+  
+
+  return (
+    <>
+      <Header />
+      <main>
+        
+      </main>
+     
+    </>
+  );
+
+}
+
+// const Home: React.FC = () => (
+//   <section className='mx-auto  py-6 bg-beige'>
+//     <h1 className='text-lg font-bold text-primary-800'>
       
-    </h1>
-    <Header />
-    <div className="text-lg">
-<Link href="/other" >
-  other
-</Link>
-</div>
-  </section>
+//     </h1>
+//     <Header />
+//     <div className="text-lg">
+// <Link href="/other" >
+//   other
+// </Link>
+// </div>
+//   </section>
       
-);
+// );
+
+
 
   
 
-export default Home;
+// export default Home;
